5d8898d8dc1b13299d6da7fc3934eec1d84b8f5a
MatCaban <caban.matus@gmail.com>
2024-03-19 22:53:12 +0100

    Added event listeners for number buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
 //selectors for all buttons
 const zeroBtn = document.querySelector("#b0");
+const oneBtn = document.querySelector("#b1");
+const twoBtn = document.querySelector("#b2");
+const threeBtn = document.querySelector("#b3");
+const fourBtn = document.querySelector("#b4");
+const fiveBtn = document.querySelector("#b5");
+const sixBtn = document.querySelector("#b6");
+const sevenBtn = document.querySelector("#b7");
+const eightBtn = document.querySelector("#b8");
+const nineBtn = document.querySelector("#b9");
+
+
+
+
 const lowerDisplay = document.querySelector(".lower-display")
 
 
@@ -9,5 +22,14 @@ function displayNumber(e){
     lowerDisplay.textContent += e.target.textContent;
 }
 
-
+//buttons listener for displaying number
 zeroBtn.addEventListener("click", displayNumber);
+oneBtn.addEventListener("click", displayNumber);
+twoBtn.addEventListener("click", displayNumber);
+threeBtn.addEventListener("click", displayNumber);
+fourBtn.addEventListener("click", displayNumber);
+fiveBtn.addEventListener("click", displayNumber);
+sixBtn.addEventListener("click", displayNumber);
+sevenBtn.addEventListener("click", displayNumber);
+eightBtn.addEventListener("click", displayNumber);
+nineBtn.addEventListener("click", displayNumber);
