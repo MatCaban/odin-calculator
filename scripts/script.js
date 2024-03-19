606f992fd0c33a76faad87dc60af980e58ebebca
MatCaban <caban.matus@gmail.com>
2024-03-19 22:46:02 +0100

    Update button IDs and add displayNumber function

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
-const zeroBtn = document.querySelector("#0");
+//selectors for all buttons
+const zeroBtn = document.querySelector("#b0");
+const lowerDisplay = document.querySelector(".lower-display")
 
-zeroBtn.addEventListener("click", (e)=>{
+
+//write number of selected button into display
+function displayNumber(e){
     console.log(e.target.textContent);
-})
+    lowerDisplay.textContent += e.target.textContent;
+}
+
+
+zeroBtn.addEventListener("click", displayNumber);
